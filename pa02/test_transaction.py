32dd92e763c3a10a1e8e78191071069b6860dead
isaacgoldings <isaacgoldings2@gmail.com>
2022-03-24 15:17:32 -0400

    added some tests, dont work yet

--- pa02/test_transaction.py
+++ pa02/test_transaction.py
@@ -1,3 +1,52 @@
  
 import pytest
 from transactions import transaction
+
+
+@pytest.fixture
+def dbfile(tmpdir):
+    ''' create a database file in a temporary file system '''
+    return tmpdir.join('test_tracker.db')
+
+@pytest.fixture
+def empty_db(dbfile):
+    db = transaction(dbfile)
+    yield db
+
+
+@pytest.fixture
+def test_db(empty_db):
+    trans1 = {'itemNum':'1', 'amount':'20', 'category':'rent', 'date':'2022-3-24', 'description':'test'}
+    trans2 = {'itemNum':'2', 'amount':'30', 'category':'food', 'date':'2021-5-24', 'description':'testa'}
+    trans3 = {'itemNum':'3', 'amount':'40', 'category':'utilities', 'date':'1492-6-29', 'description':'testb'}
+    id1=empty_db.add_transactions(trans1)
+    id2=empty_db.add_transactions(trans2)
+    id3=empty_db.add_transactions(trans3)
+    yield empty_db
+
+
+
+#test add_transactions and show_transaction
+@pytest.mark.add
+def test_add(test_db):
+    # trans0 = {'itemNum':'4', 'amount':'50', 'category':'test1', 'date':'2500-1-10', 'description':'testadd'}
+    # db = test_db.show_transactions()
+    # yield len(db)
+    # assert len(db) == 3
+    # recent = test_db.add(trans0)
+    # assert len(db) == 4
+
+    # assert recent['itemNum']=='4'
+    # assert recent['amount']=='50'
+    # assert recent['category']=='test1'
+    # assert recent['date']=='2500-1-10'
+    # assert recent['description']=='testadd'
+
+    trans0 = {'itemNum':'4', 'amount':'50', 'category':'test1', 'date':'2500-1-10', 'description':'testadd'}
+    tran0 = test_db.show_transactions()
+    rowid = test_db.add_transactions(tran0)
+    trans1 = test_db.show_transactions()
+    assert len(trans1) == len(trans0) + 1
+    tran1 = test_db.select_one(rowid)
+    assert tran1['name']==tran0['name']
+    assert tran1['desc']==tran0['desc']
